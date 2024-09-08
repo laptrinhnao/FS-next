ee7159561ad636d69f3e92b5b1016afb6e2b6012
laptrinhnao <nguyenvanhuy1.9c@gmail.com>
2024-09-08 14:28:17 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      hello
+      <div>
+        <Button type="primary">Click</Button>
+      </div>
+    </div>
+  );
+}
